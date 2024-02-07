911ad08a94bbdf551c387d45073306d79fc9d7f2
João Paulo Dias <121265378+Joaopdiasventura@users.noreply.github.com>
2024-02-07 05:41:53 -0300

    feat: liberando o cors para qualquer frontend

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import fastify from "fastify";
+import cors from '@fastify/cors';
 import startServer from "./server";
 import Route from "./route";
 
 const app = fastify({logger: true});
+
+app.register(cors, {
+    origin: "*",
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH']
+  });
 
 app.register(Route);
 
